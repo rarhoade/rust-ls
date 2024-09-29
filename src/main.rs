2a980e10131e8ecebc912446c8091c82b779f1e3
Rahul Rhoades <rhoadesrahul@gmail.com>
2024-09-29 14:56:45 -0600

    Can parse if path is directory or file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
     let paths = fs::read_dir("./").unwrap();
     for path in paths {
-        let mut path_string = path.unwrap().path().display().to_string();
+        let path_string = path.unwrap().path().display().to_string();
         let len = path_string.len();
-        path_string = path_string[2..len].parse().unwrap();
-        if !path_string.starts_with(".") {
-            println!("Name: {}", path_string)
+        let modified_path_string: String = path_string[2..len].parse().unwrap();
+        if !modified_path_string.starts_with(".") {
+            println!("Name: {}", modified_path_string);
+            let path_buf = PathBuf::from(path_string);
+            println!("is_dir: {}", path_buf.is_dir());
+            println!("is_file: {}", path_buf.is_file())
         }
     }
 }
