6c648922d1cfcb9a56289570733ee7bec37ac7f0
Rahul Rhoades <rhoadesrahul@gmail.com>
2024-09-29 14:52:04 -0600

    Showing only non hidden files

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use std::fs;
 fn main() {
     let paths = fs::read_dir("./").unwrap();
     for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
+        let mut path_string = path.unwrap().path().display().to_string();
+        let len = path_string.len();
+        path_string = path_string[2..len].parse().unwrap();
+        if !path_string.starts_with(".") {
+            println!("Name: {}", path_string)
+        }
     }
 }
